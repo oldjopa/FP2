916ee02d881dc531d706d3074cc8dac266291219
Leonid <50008736+lolkek22854@users.noreply.github.com>
2024-10-29 14:33:47 +0300

    added program.fs

--- lab2/Program.fs
+++ lab2/Program.fs
@@ -7,7 +7,9 @@ let main (argv: string array) =
 
     let tree = Empty
 
-    let tree = tree |> insert 2 |> insert 3 |> insert 3 |> insert 7 |> insert 4 |> insert 8
+    let tree =
+        tree |> insert 2 |> insert 3 |> insert 3 |> insert 7 |> insert 4 |> insert 8
+
     printTree tree ""
 
     let tree = remove 3 tree
@@ -22,14 +24,15 @@ let main (argv: string array) =
     let sum = foldRight (fun acc x -> x + acc) 0 tree
 
     let tree2 = Empty |> insert 6 |> insert 8 |> insert 3 |> insert 4
-    let mergedTree = mergeTrees tree tree2
+    let mergedTree = merge tree tree2
     printTree mergedTree ""
 
 
 
-    let tree3 = Empty |> insert 5 |> insert 3 |> insert 7 |> insert 3 |> insert 6 |> insert 8
+    let tree3 =
+        Empty |> insert 5 |> insert 3 |> insert 7 |> insert 3 |> insert 6 |> insert 8
+
     printTree tree3 "-"
     let updatedTree = remove 5 tree3
     printTree updatedTree "="
     0
-
