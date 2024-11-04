b23bb9333f723c9605637ce90475f4a67b08f778
Leonid <50008736+lolkek22854@users.noreply.github.com>
2024-11-04 19:25:38 +0300

    Учтены правки преподавателя: Added BTree arbitray

--- lab2tests/PropertyTests.fs
+++ lab2tests/PropertyTests.fs
@@ -9,23 +9,31 @@ open bt_bag
 type ArrayArbitrary() =
     static member Arb =
         Arb.fromGen (
-            Gen.choose (50, 100)
+            Gen.choose (0, 100)
             |> Gen.map (fun length -> Gen.arrayOfLength length (Gen.choose (-50, 50)))
         )
 
+type ArbitraryBTreeMultiset<'T when 'T : comparison>() =
+    static member BTree() =
+        Arb.fromGen <| 
+            gen {
+                let! size = Gen.choose (10, 50)
+                let! values = Gen.listOfLength size (Arb.generate<'T>)
+                return List.fold (fun acc n -> insert n acc) Empty values
+            }
 
-[<Property(Arbitrary = [| typeof<ArrayArbitrary> |])>]
-let prop_emptyMerge (n: int[]) =
-    let newBag = Array.fold (fun acc x -> insert x acc) Empty n
-    compare newBag (merge newBag Empty)
+[<Property(Arbitrary = [| typeof<ArbitraryBTreeMultiset<int>> |])>]
+let prop_emptyMerge (bag: BTreeMultiset<int>) =
+
+    compare bag (merge bag Empty)
 
 
 
-[<Property(Arbitrary = [| typeof<ArrayArbitrary> |])>]
-let prop_associativity (n1: int[]) (n2: int[]) (n3: int[]) =
-    let bag1 = Array.fold (fun acc x -> insert x acc) Empty n1
-    let bag2 = Array.fold (fun acc x -> insert x acc) Empty n2
-    let bag3 = Array.fold (fun acc x -> insert x acc) Empty n3
+[<Property(Arbitrary = [| typeof<ArbitraryBTreeMultiset<int>> |])>]
+let prop_associativity (bag1: BTreeMultiset<int>) (bag2: BTreeMultiset<int>) (bag3: BTreeMultiset<int>) =
+    // let bag1 = Array.fold (fun acc x -> insert x acc) Empty n1
+    // let bag2 = Array.fold (fun acc x -> insert x acc) Empty n2
+    // let bag3 = Array.fold (fun acc x -> insert x acc) Empty n3
     let mergedBag = merge bag1 (merge bag2 bag3)
     let mergedBag2 = merge (merge bag1 bag2) bag3
     compare mergedBag mergedBag2
